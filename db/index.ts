662478e3129a83982ca9029e7888b2b9e0a1fbc8
bogaledemasrepo <bogidemas@gmail.com>
2026-05-02 04:45:31 +0000

    ENV variable name chnge

--- db/index.ts
+++ db/index.ts
@@ -2,5 +2,5 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 
-const queryClient = postgres(process.env.DIGITALEQUB_API_DATABASE_LOCAL_URL!);
+const queryClient = postgres(process.env.DATABASE_URL!);
 export const db = drizzle(queryClient, { schema });         
